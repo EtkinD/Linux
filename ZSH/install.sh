ed045b72941785d3c7fef0419118cfff40b89cea
Etkin Dogan <62807955+EtkinD@users.noreply.github.com>
2025-04-02 00:31:00 +0300

    Change ohmyzsh installation script

--- ZSH/install.sh
+++ ZSH/install.sh
@@ -20,7 +20,7 @@ chsh -s $(which zsh)
 
 ## --------------------------------------------------
 echo "Installing oh-my-zsh and powerlevel10k..."
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 ## --------------------------------------------------
